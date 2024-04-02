216f0cfafcf3d9a1ca8e34d6b4af13f5964b1939
Fatih Ozer <fatihozer246@gmail.com>
2024-04-02 12:00:39 +0200

    add connectionstring

--- ServiceBusApp.Common/Constants.cs
+++ ServiceBusApp.Common/Constants.cs
@@ -8,7 +8,8 @@ namespace ServiceBusApp.Common
 {
 	public static class Constants
 	{
-		public const string ConnectionString = "Connection";
+		public const string ConnectionString = "Endpoint=sb://fatihozerservicebusdemo.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=rQQbhP+9jqKck77qCHImX3dAiooGNP/dQ+ASbIsaOPI=";
+
 
 		public const string OrderCreatedQueueName = "OrderCreatedQueue";
 		public const string OrderDeletedQueueName = "OrderDeletedQueue";
